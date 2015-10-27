5fd47026b875a8879ba28e9da87955d94c7b1bcc
Malte Woelky <m.woelky@babiel.com>
2015-10-27 19:23:23 +0100

    M101J-2: FindWithFilterTest: insert and print

---
+++ src/main/java/com/babiel/test/mongodb/m101j/week2/FindWithFilterTest.java
@@ -0,0 +1,40 @@
+package com.babiel.test.mongodb.m101j.week2;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.babiel.test.mongodb.m101j.util.Helpers.printJson;
+
+public class FindWithFilterTest {
+  public static void main(String[] args) {
+    MongoClient client = new MongoClient();
+    MongoDatabase db = client.getDatabase("course");
+    MongoCollection<Document> collection = db.getCollection("findWithFilterTest");
+
+    collection.drop();
+
+    //insert 10 documents with two random integers
+    for (int i = 0; i < 10; i++) {
+      collection.insertOne(new Document()
+          .append("x", new Random().nextInt(2))
+          .append("y", new Random().nextInt(100)));
+    }
+
+    List<Document> all = collection.find().into(new ArrayList<Document>());
+
+    for (Document cur : all) {
+      printJson(cur);
+    }
+
+    long count = collection.count();
+    System.out.println();
+    System.out.println(count);
+  }
+}
